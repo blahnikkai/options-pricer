475c94be0b9a888dab8ef3a682672c51019224fc
blahnikkai <83978042+blahnikkai@users.noreply.github.com>
2025-07-21 22:42:13 -0400

    Add hedging calculations (underlying price, share count, bank balance).

--- main.cpp
+++ main.cpp
@@ -9,10 +9,16 @@ struct Moment {
 };
 
 double calculate_option_price(
-    int steps, double start_ulying, double strike, double q, double u, double risk_free_rate, double timestep)
+    int steps, double start_ulying, double strike, double u, double risk_free_rate, double timestep)
 {
+    double d = 1 / u;
     vector<vector<Moment>> moments;
     vector<Moment> final_moments;
+    // continuous
+    // double lambda = exp(timestep * risk_free_rate);
+    // discrete
+    double lambda = 1 + risk_free_rate;
+    double q = (lambda * u - 1) / (u * u - 1);
     for(int i = 0; i <= steps; i++) {
         int worst = -steps;
         double underlying = start_ulying * pow(u, worst + 2 * i);
@@ -27,14 +33,13 @@ double calculate_option_price(
         for(int j = 0; j < steps - i; j++) {
             Moment d_moment = moments.at(i).at(j);
             Moment u_moment = moments.at(i).at(j + 1);
-            double rate_discount = exp(-timestep * risk_free_rate);
-            double option_price = rate_discount * (q * u_moment.option_price + (1 - q) * d_moment.option_price);
-            // double underlying = q * u_moment.underlying + (1 - q) * d_moment.underlying;
-            double underlying = -1;
-            double shares = -1;
-            double bank_bal = -1;
+            double option_price =  1 / lambda * (q * u_moment.option_price + (1 - q) * d_moment.option_price);
+            double underlying = 1 / lambda * (q * u_moment.underlying + (1 - q) * d_moment.underlying);
+            double m = u_moment.option_price;
+            double n = d_moment.option_price;
+            double shares = (m - n) / (underlying * (u - d));
+            double bank_bal = (u * n - m * d) / (lambda * (u - d));
             moment_row.push_back({option_price, underlying, shares, bank_bal});
-
         }
         moments.push_back(moment_row);
     }
@@ -46,11 +51,10 @@ int main() {
     int steps = 3;
     double start_ulying = 100;
     double strike = 100;
-    double q = 0.5286;
     double u = 1.1;
     double risk_free_rate = 0.01;
     double timestep = 1;
-    double option_price = calculate_option_price(steps, start_ulying, strike, q, u, risk_free_rate, timestep);
+    double option_price = calculate_option_price(steps, start_ulying, strike, u, risk_free_rate, timestep);
     cout << option_price << '\n';
     return 0;
 }
