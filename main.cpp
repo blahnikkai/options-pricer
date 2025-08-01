9a62642a40a5c4bdb7fd5b75b43f06725fd50093
blahnikkai <83978042+blahnikkai@users.noreply.github.com>
2025-07-31 21:14:01 -0400

    Add function calc_vol_from_desired_u() and and reorder the parameters of calc_binomial() to match the future black scholes function.

--- main.cpp
+++ main.cpp
@@ -8,16 +8,19 @@ struct Moment {
     double bank_bal;
 };
 
-double calculate_option_price(
-    int steps, double start_ulying, double strike, double u, double risk_free_rate, double timestep)
+// time values (time_to_expiry, risk_free_rate, volatility) should be in the same units
+double calc_binomial(
+    double start_ulying, double strike, double time_to_expiry, double risk_free_rate, double vol, int steps)
 {
+    double timestep = time_to_expiry / steps;
+    double u = exp(vol * timestep);
     double d = 1 / u;
     vector<vector<Moment>> moments;
     vector<Moment> final_moments;
     // continuous
     // double lambda = exp(timestep * risk_free_rate);
     // discrete
-    double lambda = 1 + risk_free_rate;
+    double lambda = pow(1 + risk_free_rate, timestep);
     double q = (lambda * u - 1) / (u * u - 1);
     for(int i = 0; i <= steps; i++) {
         int worst = -steps;
@@ -47,14 +50,21 @@ double calculate_option_price(
     return calculated_price;
 }
 
+double calc_vol_from_desired_u(double u, double timestep) {
+    return log(u) / sqrt(timestep);
+}
+
 int main() {
-    int steps = 3;
     double start_ulying = 100;
     double strike = 100;
-    double u = 1.1;
+    double time_to_expiry = 3;
     double risk_free_rate = 0.01;
-    double timestep = 1;
-    double option_price = calculate_option_price(steps, start_ulying, strike, u, risk_free_rate, timestep);
+
+    int steps = 3;
+    double u = 1.1;
+    double vol = calc_vol_from_desired_u(u, time_to_expiry / steps);
+
+    double option_price = calc_binomial(start_ulying, strike, time_to_expiry, risk_free_rate, vol, steps);
     cout << option_price << '\n';
     return 0;
 }
