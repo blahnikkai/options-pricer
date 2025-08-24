1c8b1fd21ab77a7f271e8eae64560cbd544358df
blahnikkai <83978042+blahnikkai@users.noreply.github.com>
2025-08-23 20:09:09 -0400

    Add build and run tips in the README.

--- README.md
+++ README.md
@@ -4,10 +4,24 @@
 - Calculate exact values of options contracts with the Black-Scholes formula.  
 - Approximate values of options with a binomial tree or Monte Carlo simulation.
 
-## To Run
+## Building
 
-Run `make cli` to build the cli executable.
+```zsh
+mkdir build
+cd build
+cmake .
+make options_pricer_cli
+```
 
-### Example Run
+## Running
 
+The below will run the Black-Scholes formula for a European call option 
+1. Underlying price 100.30
+2. Strike price 105.50
+3. 2.4 units of time to expiry
+4. A risk free rate of return of 4.5 percent per unit time
+5. An underlying volatility of 0.65 (standard deviation of log returns over time horizon = 1 unit of time)
 
+```zsh
+./options_pricer_cli -u 100.30 -s 105.50 -t 2.4 -r 4.5 -v 0.65
+```
--- src/cli.cpp
+++ src/cli.cpp
@@ -12,31 +12,31 @@ int main(int argc, char ** argv) {
     arg_parser
         .add_argument("-u", "--underlying")
         .required()
-        .help("The starting price of the underlying asset")
+        .help("The starting price of the underlying asset.")
         .store_into(u);
 
     arg_parser
         .add_argument("-s", "--strike")
         .required()
-        .help("The strike price of the option")
+        .help("The strike price of the option.")
         .store_into(k);
 
     arg_parser
         .add_argument("-t", "--time-to-expiry")
         .required()
-        .help("The amount of time until the option expires")
+        .help("The amount of time until the option expires.")
         .store_into(t);
 
     arg_parser
         .add_argument("-r", "--risk-free-rate")
         .required()
-        .help("The rate of return on a risk free investment")
+        .help("The rate of return as a percent per unit time on a risk free investment.")
         .store_into(r);
 
     arg_parser
         .add_argument("-v", "--volatility")
         .required()
-        .help("The volatility of the underlying asset")
+        .help("The volatility of the underlying asset with time horizon equal to a unit time.")
         .store_into(v);
 
     try {
@@ -47,6 +47,8 @@ int main(int argc, char ** argv) {
         std::cerr << arg_parser;
         std::exit(1);
     }
+
+    r /= 100;
 
     double black_scholes = pricing::calc_black_scholes(u, k, t, r, v);
     std::cout << black_scholes << "\n";
